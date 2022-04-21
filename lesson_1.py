153854ef3f6c1be1acc31ccd4c079231cc0f344b
RentedGiraffe <87725326+RentedGiraffe@users.noreply.github.com>
2022-04-20 18:01:20 -0700

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,58 @@
+from BirdBrain import Finch
+import time
+
+bird = Finch()
+
+def exercise1():
+    bird.setMove('F',15,50)
+
+    
+def exercise2():
+    bird.setMove('F',15,50)
+
+    bird.setMove('B',15,50)
+
+    bird.setMove('F',15,50)
+
+    bird.setMove('B',30,50)
+
+
+def exercise3():
+    bird.setTurn('L',360,50)
+
+    bird.setTurn('R',360,49)
+
+
+def exercise4():
+    bird.setMove('F',10,50)
+    bird.setTurn('R',90,50)
+    bird.setMove('F',10,50)
+    bird.setTurn('R',90,50)
+    bird.setMove('F',10,50)
+    bird.setTurn('R',90,50)
+    bird.setMove('F',10,50)
+    bird.setTurn('R',90,50)
+
+
+def exercise5():
+    bird.setMove('F',10,50)
+    bird.setTurn('R',120,50)
+    bird.setMove('F',10,50)
+    bird.setTurn('R',120,50)
+    bird.setMove('F',10,50)
+    bird.setTurn('R',120,50)
+
+
+def exercise6():
+    bird.setMove('F',10,50)
+    bird.setTurn('R',90,50)
+    bird.setMove('F',10,50)
+    bird.setTurn('L',90,50)
+    bird,setMove('F',10,50)
+    bird.setTurn('L',90,50)
+    bird.setMove('F',10,50)
+
+                 
+exercise6()
+
+
